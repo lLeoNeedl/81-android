1bc1a56a8bfe06ec348ee1f32d21d52c051a7da8
Leonid Lavrov <274852@gmail.com>
2023-05-13 18:16:01 +0300

    Making layout for first game

--- app/src/main/java/com/nile/fortu/game/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/nile/fortu/game/EnterPhoneNumberFragment.kt
@@ -1,13 +1,16 @@
 package com.nile.fortu.game
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 
 class EnterPhoneNumberFragment : Fragment() {
 
+    lateinit var flContinue: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +25,14 @@ class EnterPhoneNumberFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_enter_phone_number, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        flContinue = view.findViewById(R.id.fl_continue)
+
+        flContinue.setOnClickListener {
+            val intent = Intent(requireContext(), FirstGameActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
---
+++ app/src/main/java/com/nile/fortu/game/FirstGameActivity.kt
@@ -0,0 +1,11 @@
+package com.nile.fortu.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_game)
+    }
+}
